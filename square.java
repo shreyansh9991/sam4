9823b8e6ecfdf841b9178df887cee6de6e1d49a1
temporary987 <satok@itfast.net>
2019-09-27 13:11:17 +0530

    pehla commit

---
+++ square.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class square{
+	public static void main(String args[0]){
+		Scanner sc = new Scanner(System.in);
+		System.out.in("Enter the number :");
+		n = sc.nextInt();
+		sq=n*n;
+		System.out.println("The answer is :"+sq);
+	}
+}
