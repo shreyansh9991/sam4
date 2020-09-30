b0834560a1bc2972100cd4d5da5a1f48d1aa9c42
tanishverma <31825971+tanishverma@users.noreply.github.com>
2020-09-30 23:21:44 +0530

    Update square.java

--- square.java
+++ square.java
@@ -5,7 +5,7 @@ class square{
 		System.out.in("Enter the number :");
 		n = sc.nextInt();
 		sq=n*n;
-		System.out.println("The Answer is:")
+		System.out.println("The Answer is:");
 		System.out.println(sq);
 	}
 }
